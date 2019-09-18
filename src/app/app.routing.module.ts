956018eb5b7fa44d33f6daba54281f00cef129a1
Vincent Roth <vroth@sqli.com>
2019-09-18 09:37:25 +0200

    Practice 8 - child route

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,15 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
-import { BookListComponent } from './consultation/book-list/book-list.component';
-import { BookDetailComponent } from './consultation/book-detail/book-detail.component';
 
 const routes: Routes = [
-  { path: '', component: BookListComponent },
-  {
-    path: 'book/:id',
-    component: BookDetailComponent
-  }
+  { path: '', redirectTo: 'consultation', pathMatch: 'full' }
 ];
 
 @NgModule({
--- src/app/consultation/consultation.module.ts
+++ src/app/consultation/consultation.module.ts
@@ -4,11 +4,31 @@ import { BookComponent } from './book/book.component';
 import { BookListComponent } from './book-list/book-list.component';
 import { SharedModule } from '../shared/shared.module';
 import { BookDetailComponent } from './book-detail/book-detail.component';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { DevantureComponent } from './devanture/devanture.component';
+
+const routes: Routes = [
+  {
+    path: 'consultation',
+    component: DevantureComponent,
+    children: [
+      { path: '', component: BookListComponent },
+      {
+        path: 'book/:id',
+        component: BookDetailComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
-  declarations: [BookComponent, BookListComponent, BookDetailComponent],
-  imports: [CommonModule, SharedModule, RouterModule],
-  exports: [BookListComponent]
+  declarations: [
+    BookComponent,
+    BookListComponent,
+    BookDetailComponent,
+    DevantureComponent
+  ],
+  imports: [CommonModule, SharedModule, RouterModule.forChild(routes)],
+  exports: []
 })
 export class ConsultationModule {}
