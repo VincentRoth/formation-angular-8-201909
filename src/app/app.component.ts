dfe707232313c38b5d0b0b24ba48378e449bc5a4
Vincent Roth <vroth@sqli.com>
2019-09-19 09:47:43 +0200

    Environments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'formation-angular8';
+
+  getEnvironment(): string {
+    if (environment.production) {
+      return 'PRODUCTION';
+    }
+    return 'DEVELOPMENT';
+  }
 }
